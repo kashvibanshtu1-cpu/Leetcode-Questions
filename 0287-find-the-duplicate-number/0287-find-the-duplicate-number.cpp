bcf397aeddf8e2611c968dcd816902fc69ba8637
Kashvi 😎 <kashvibanshtu1@gmail.com>
2026-06-28 18:56:02 +0530

    Time: 0 ms (100%), Space: 64.9 MB (90.81%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
@@ -1,22 +1,22 @@
 class Solution {
 public:
     int findDuplicate(vector<int>& arr) {
-        int slow =0;
-        int fast =0;
-        while(true){
-            slow= arr[slow];
+        int slow = 0;
+        int fast = 0;
+        while (true) {
+            slow = arr[slow];
             fast = arr[arr[fast]];
-            if(slow==fast){
-            slow =0;
-            while(true ){
-                slow= arr[slow];
-                fast = arr[fast];
-                if(slow==fast){
-                    return slow ;
+            if (slow == fast) {
+                slow = 0;
+                while (true) {
+                    slow = arr[slow];
+                    fast = arr[fast];
+                    if (slow == fast) {
+                        return slow;
+                    }
                 }
             }
-            }
         }
-        return -1 ;
+        return -1;
     }
 };
