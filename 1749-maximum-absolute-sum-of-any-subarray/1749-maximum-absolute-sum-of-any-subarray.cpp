46b97a85db45d29d9be1f19c4b3ca063d4b5a96c
Kashvi 😎 <kashvibanshtu1@gmail.com>
2026-06-29 19:15:07 +0530

    Time: 4 ms (9.38%), Space: 45 MB (88.64%) - LeetHub

--- 1749-maximum-absolute-sum-of-any-subarray/1749-maximum-absolute-sum-of-any-subarray.cpp
+++ 1749-maximum-absolute-sum-of-any-subarray/1749-maximum-absolute-sum-of-any-subarray.cpp
@@ -3,17 +3,15 @@ public:
     int maxAbsoluteSum(vector<int>& nums) {
         int minsum = nums[0];
         int maxsum = nums[0];
-        int Minans = nums[0];
-        int Maxans =nums[0];
-        for(int i=1;i<nums.size();i++){
-            int v1= nums[i];
-            int v2= minsum +nums[i];
-            int v3= maxsum + nums[i];
-            minsum = min(v1,v2);
-            maxsum = max(v1,v3);
-            Minans = min(minsum,Minans);
-            Maxans = max(Maxans,maxsum);
+        int ans = nums[0];
+        for (int i = 1; i < nums.size(); i++) {
+            int v1 = nums[i];
+            int v2 = minsum + nums[i];
+            int v3 = maxsum + nums[i];
+            minsum = min(v1, min(v3, v2));
+            maxsum = max(v1, max(v2, v3));
+            ans = max(ans, max(abs(minsum), maxsum));
         }
-        return max(abs(Minans),Maxans);
+        return abs(ans);
     }
 };
