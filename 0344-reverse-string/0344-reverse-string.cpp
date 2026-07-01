3c0e91be3cb53aca035ec869c69f60f59d195f06
Kashvi 😎 <kashvibanshtu1@gmail.com>
2026-07-01 11:35:15 +0530

    Time: 0 ms (100%), Space: 27.1 MB (80.76%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.cpp
+++ 0344-reverse-string/0344-reverse-string.cpp
@@ -1,15 +1,23 @@
 class Solution {
 public:
     void reverseString(vector<char>& s) {
-        stack<char> st ;
-        for(int i=0;i<s.size();i++){
-            st.push(s[i]);
+        // stack<char> st ;
+        // for(int i=0;i<s.size();i++){
+        //     st.push(s[i]);
+        // }
+        // for(int i=0;i<s.size();i++){
+        //     char ch = st.top();
+        //     st.pop();
+        //     s[i]= ch;
+        // }
+
+        int left = 0;
+        int right = s.size() - 1;
+
+        while (left < right) {
+            swap(s[left], s[right]);
+            left++;
+            right--;
         }
-        for(int i=0;i<s.size();i++){
-            char ch = st.top();
-            st.pop();
-            s[i]= ch;
-        }
-       
     }
 };
