b4837b9976504c82cae030d325cbef35e48c1d8c
Kashvi 😎 <kashvibanshtu1@gmail.com>
2026-07-01 11:19:40 +0530

    Time: 3 ms (9.22%), Space: 9 MB (18.68%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+        for (int i = 0; i < s.size(); i++) {
+            if (s[i] == '(' || s[i] == '{' || s[i] == '[') {
+                st.push(s[i]);
+            }
+            if (st.empty()) {
+                return false;
+            }
+            if ((s[i] == '}' && st.top() == '{') ||
+                (s[i] == ')' && st.top() == '(') ||
+                (s[i] == ']' && st.top() == '[')) {
+                st.pop();
+            } else if ((s[i] == '}' && st.top() != '{') ||
+                       (s[i] == ')' && st.top() != '(') ||
+                       (s[i] == ']' && st.top() != '[')) {
+                return false;
+            }
+        }
+        if (!st.empty()) {
+            return false;
+        }
+        return true;
+    }
+};
