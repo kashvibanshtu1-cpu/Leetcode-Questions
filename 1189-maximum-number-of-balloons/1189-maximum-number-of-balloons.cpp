29869b0120f847179d25d9eb37280a849c02d43d
Kashvi 😎 <kashvibanshtu1@gmail.com>
2026-07-04 19:40:58 +0530

    Time: 0 ms (100%), Space: 9 MB (21.15%) - LeetHub

--- 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.cpp
+++ 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.cpp
@@ -1,22 +1,25 @@
 class Solution {
 public:
     int maxNumberOfBalloons(string text) {
-        unordered_map<char, int> mp;
+        int ans = INT_MAX;
+        unordered_map<char, int> text_mp;
         for (int i = 0; i < text.size(); i++) {
-            mp[text[i]]++;
+            text_mp[text[i]]++;
         }
         string b = "balloon";
-        unordered_map<char, int> bmp;
+        unordered_map<char, int> balloon_mp;
         for (int i = 0; i < b.size(); i++) {
-            bmp[b[i]]++;
+            balloon_mp[b[i]]++;
         }
-        int ans = INT_MAX;
-        for (auto p : bmp) {
+        for (auto p : balloon_mp) {
             char c = p.first;
             int deno = p.second;
-            int num = mp[c];
-            int times = num/deno;
+            int nums = text_mp[c];
+            int times = nums / deno;
             ans = min(ans, times);
+        }
+        if (ans == INT_MAX) {
+            return 0;
         }
         return ans;
     }
