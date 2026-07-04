2df69672332e8ec8bec8927f07b6fce8f609ceff
Kashvi 😎 <kashvibanshtu1@gmail.com>
2026-07-04 19:49:57 +0530

    Time: 0 ms (100%), Space: 9.5 MB (17.87%) - LeetHub

--- 0409-longest-palindrome/0409-longest-palindrome.cpp
+++ 0409-longest-palindrome/0409-longest-palindrome.cpp
@@ -1,38 +1,24 @@
 class Solution {
 public:
     int longestPalindrome(string s) {
-        int n = s.size();
-        int count = 0;
         unordered_map<char, int> mp;
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < s.size(); i++) {
             mp[s[i]]++;
         }
+        int count = 0;
         bool odd = false;
-        for (auto i : mp) {
-            int freq = i.second;
+        for (auto p : mp) {
+            int freq = p.second;
             if (freq % 2 == 0) {
                 count += freq;
             } else {
                 odd = true;
-                int val = i.second;
-                if (val % 2 != 0) {
-                    int freq = val - 1;
-                    count += freq;
-                }
+                count += freq - 1;
             }
         }
-            if (odd == false) {
-                return count;
-            }
-            // if (odd == true) {
-            //     for (auto i : mp) {
-            //         int val = i.second;
-            //         if (val % 2 != 0) {
-            //             int freq = val - 1;
-            //             count += freq;
-            //         }
-            //     }
-            
+        if (odd == true) {
             return count + 1;
         }
-    };
+        return count;
+    }
+};
