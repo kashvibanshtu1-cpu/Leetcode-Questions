978247bd79087f7552582d647b680021c61b97ab
Kashvi 😎 <kashvibanshtu1@gmail.com>
2026-07-14 15:31:13 +0530

    Time: 0 ms (100%), Space: 13.3 MB (45.07%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -3,34 +3,50 @@ public:
     bool searchMatrix(vector<vector<int>>& arr, int tar) {
         int rowno = arr.size();
         int colno = arr[0].size();
+        // int start = 0;
+        // int end = rowno - 1;
+        // int row = -1;
+        // while (start <= end) {
+        //     int mid = start + (end - start) / 2;
+        //     if (arr[mid][0] == tar) {
+        //         return true;
+        //     }
+        //     else if (arr[mid][0] > tar) {
+        //         end = mid - 1;
+        //     } else {
+        //         row = mid ;
+        //         start = mid + 1;
+        //     }
+        // }
+        // if(row == -1){
+        //     return false ;
+        // }
+        // int star = 0;
+        // int ed = colno-1;
+        // while (star <= ed) {
+        //     int med = star + (ed - star) / 2;
+        //     if (arr[row][med] == tar) {
+        //         return true;
+        //     } else if (arr[row][med] > tar) {
+        //         ed = med - 1;
+        //     } else {
+        //         star = med + 1;
+        //     }
+        // }
+        // return false;
         int start = 0;
-        int end = rowno - 1;
-        int row = -1;
+        int end = rowno * colno - 1;
         while (start <= end) {
             int mid = start + (end - start) / 2;
-            if (arr[mid][0] == tar) {
+            int row = mid / colno;
+            int col = mid % colno;
+            if (arr[row][col] == tar){
                 return true;
             }
-            else if (arr[mid][0] > tar) {
+            else if (arr[row][col] > tar) {
                 end = mid - 1;
             } else {
-                row = mid ;
                 start = mid + 1;
-            }
-        }
-        if(row == -1){
-            return false ;
-        }
-        int star = 0;
-        int ed = colno-1;
-        while (star <= ed) {
-            int med = star + (ed - star) / 2;
-            if (arr[row][med] == tar) {
-                return true;
-            } else if (arr[row][med] > tar) {
-                ed = med - 1;
-            } else {
-                star = med + 1;
             }
         }
         return false;
