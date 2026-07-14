f0f32a2a6ec59e60451ca0fb372bd36c83987765
Kashvi 😎 <kashvibanshtu1@gmail.com>
2026-07-14 15:23:23 +0530

    Time: 0 ms (100%), Space: 13.4 MB (7.4%) - LeetHub

---
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& arr, int tar) {
+        int rowno = arr.size();
+        int colno = arr[0].size();
+        int start = 0;
+        int end = rowno - 1;
+        int row = -1;
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (arr[mid][0] == tar) {
+                return true;
+            }
+            else if (arr[mid][0] > tar) {
+                end = mid - 1;
+            } else {
+                row = mid ;
+                start = mid + 1;
+            }
+        }
+        if(row == -1){
+            return false ;
+        }
+        int star = 0;
+        int ed = colno-1;
+        while (star <= ed) {
+            int med = star + (ed - star) / 2;
+            if (arr[row][med] == tar) {
+                return true;
+            } else if (arr[row][med] > tar) {
+                ed = med - 1;
+            } else {
+                star = med + 1;
+            }
+        }
+        return false;
+    }
+};
