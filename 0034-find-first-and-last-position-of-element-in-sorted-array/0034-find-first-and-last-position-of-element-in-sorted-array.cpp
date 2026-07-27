abddfbb36c1147cf1af1ba095661cb1864466a42
Kashvi 😎 <kashvibanshtu1@gmail.com>
2026-07-28 04:33:05 +0530

    Time: 0 ms (100%), Space: 17.7 MB (20.39%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,40 +1,46 @@
 class Solution {
 public:
     vector<int> searchRange(vector<int>& nums, int tar) {
-        int start = 0;
+        int start =0;
         int end = nums.size()-1;
-        int first = INT_MAX;
-        int second = -1;
+        int first = INT_MAX ;
         vector<int> ans;
-        while (start <= end) {
-            int mid = start + (end - start) / 2;
-            if (nums[mid] == tar) {
-                first = min(first, mid);
-                end = mid - 1;
-            } else if (nums[mid] > tar) {
-               end = mid - 1; 
-            } else {
-                start = mid + 1;
+        while(start<=end){
+            int mid = start +(end-start)/2 ;
+            if(nums[mid]== tar){
+                first = min(first,mid);
+                end = mid-1;
+            }
+            else if(nums[mid]>tar){
+                end = mid-1;
+            }
+            else{
+                start = mid+1;
             }
         }
-        int start1 = 0;
-        int end1 = nums.size()-1;
-        while (start1 <= end1) {
-            int mid = start1 + (end1 - start1) / 2;
-            if (nums[mid] == tar) {
-                second = max(second, mid);
-                start1 = mid + 1;
-            } else if (nums[mid] > tar) {
-                end1 = mid - 1;
-            } else {
-               start1 = mid + 1; 
+        int star = 0;
+        int ed = nums.size()-1 ;
+        int second = INT_MIN ;
+        while(star <= ed){
+            int med = star +(ed-star)/2 ;
+           if(nums[med]== tar){
+                second = max(second,med);
+                star = med+1;
+            }
+            else if(nums[med]>tar){
+                ed = med-1;
+            }
+            else{
+                star = med+1;
             }
         }
-        if(first == INT_MAX){
-            first =-1;
-        }
+        if(first == INT_MAX && second == INT_MIN ){
+            ans.push_back(-1);
+            ans.push_back(-1);
+        }else{
         ans.push_back(first);
         ans.push_back(second);
-        return ans;
+        }
+        return ans ;
     }
 };
