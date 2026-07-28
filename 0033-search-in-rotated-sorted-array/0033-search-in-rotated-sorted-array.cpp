0557e93681a342c5d0b3d6449aa09bb73087684c
Kashvi 😎 <kashvibanshtu1@gmail.com>
2026-07-28 06:09:12 +0530

    Time: 0 ms (100%), Space: 15.4 MB (36.52%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -1,35 +1,37 @@
 class Solution {
 public:
     int search(vector<int>& nums, int tar) {
-        int n = nums.size();
         int start = 0;
+        int n = nums.size();
         int end = n - 1;
+        int first = nums[0];
+        int last = nums[n - 1];
         while (start <= end) {
             int mid = start + (end - start) / 2;
             if (nums[mid] == tar) {
                 return mid;
             }
-            // PART2
-            if (nums[mid] > nums[n - 1]) {
-                if (nums[mid] < tar) {
-                    start = mid+1;
-                } else {
-                    if (tar >= nums[0]) {
-                        end = mid - 1;
-                    } else {
-                        start = mid + 1;
-                    }
-                }
-            } // PART1
-            else {
+            // PART 1
+            if (nums[mid] < first) {
                 if (nums[mid] > tar) {
                     end = mid - 1;
-                }
-                if (nums[mid] < tar) {
-                    if (tar <= nums[n - 1]) {
+                } else {
+                    if (tar <= last) {
                         start = mid + 1;
                     } else {
                         end = mid - 1;
+                    }
+                }
+            }
+            // Part 2
+            else {
+                if (tar > nums[mid]) {
+                    start = mid + 1;
+                } else {
+                    if (tar >= first) {
+                        end = mid - 1;
+                    } else {
+                        start = mid + 1;
                     }
                 }
             }
