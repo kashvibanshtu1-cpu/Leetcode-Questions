9e8eccd8f23cc250f0e63cb71d109f8c0d12456e
Kashvi 😎 <kashvibanshtu1@gmail.com>
2026-07-29 04:22:18 +0530

    Time: 31 ms (14.54%), Space: 70.6 MB (12.61%) - LeetHub

--- 1482-minimum-number-of-days-to-make-m-bouquets/1482-minimum-number-of-days-to-make-m-bouquets.cpp
+++ 1482-minimum-number-of-days-to-make-m-bouquets/1482-minimum-number-of-days-to-make-m-bouquets.cpp
@@ -1,43 +1,38 @@
 class Solution {
 public:
-    bool canmake(vector<int>& days, int m, int k, int mid) {
-        int count = 0;
-        int bouquet = 0;
-        for (int i = 0; i < days.size(); i++) {
-            if (days[i] <= mid) {
+    bool willbloom(vector<int>& bloom, int guess, int m, int k) {
+        int count =0;
+        int bouquet =0;
+        for (int i = 0; i < bloom.size(); i++) {
+            if (bloom[i] <= guess) {
                 count++;
-                if (count == k) {
-                    bouquet++;
-                    count = 0;
+                if (count% k == 0) {
+                   bouquet++;
                 }
-            } else {
-                count = 0;
+            }
+            else{
+                count =0;
             }
         }
-        if (bouquet >= m) {
+        if (bouquet >=m)
             return true;
-        }
         return false;
     }
-    int minDays(vector<int>& days, int m, int k) {
-        long long pro = 1LL*m*k;
-        if (days.size() < pro) {
-            return -1;
-        }
-        int start = INT_MAX, end = -1;
-        int ans = -1;
-        for (int i = 0; i < days.size(); i++) {
-            start = min(start, days[i]);
-            end = max(end, days[i]);
-        }
+    int minDays(vector<int>& bloom, int m, int k) {
+        int start = *min_element(bloom.begin(), bloom.end());
+        int end = *max_element(bloom.begin(), bloom.end());
+        int ans = INT_MAX;
         while (start <= end) {
-            int mid = start + (end - start) / 2;
-            if (canmake(days, m, k, mid)) {
-                ans = mid;
-                end = mid - 1;
+            int guess = start + (end - start) / 2;
+            if (willbloom(bloom, guess, m, k)) {
+                ans = min(ans, guess);
+                end = guess - 1;
             } else {
-                start = mid + 1;
+                start = guess + 1;
             }
+        }
+        if(ans == INT_MAX ){
+            return -1 ;
         }
         return ans;
     }
