a96ada72b978c390bf765cf28063c4cd552594b6
Kashvi 😎 <kashvibanshtu1@gmail.com>
2026-07-28 19:45:56 +0530

    Time: 2 ms (96.37%), Space: 22.9 MB (84.02%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
@@ -1,34 +1,31 @@
 class Solution {
 public:
-    bool isenough(vector<int> piles, int gaurd, int mid) {
-        long long hour = 0;
-        for (int i = 0; i < piles.size(); i++) {
-            hour += piles[i] / mid;
-            if (piles[i] % mid != 0) {
-                hour++;
+    long long speed(vector<int>& piles, int guess){
+        long long hours =0;
+        for(int i=0;i<piles.size();i++){
+           hours += piles[i]/guess ;
+           if (piles[i] % guess != 0) {
+                hours++;
             }
         }
-        if (hour > gaurd) {
-            return false;
-        }
-        return true;
+        return hours ;
     }
-    int minEatingSpeed(vector<int>& piles, int gaurd) {
-        int start = 1;
-        int end =0;
-        int ans = -1;
-        for (int i = 0; i < piles.size(); i++) {
-            end = max(end, piles[i]);
-        }
-        while (start <= end) {
-            int mid = start + (end - start) / 2;
-            if (isenough(piles, gaurd, mid)) {
-                ans = mid;
-                end = mid - 1;
-            } else {
-                start = mid + 1;
+    int minEatingSpeed(vector<int>& piles, int h) {
+        int start =1 ;
+        int end = *max_element(piles.begin(),piles.end());
+        int ans = INT_MAX ;
+        while(start<=end){
+            int guess = start+(end-start)/2 ;
+            long long hours = speed(piles,guess);
+            if(hours >h){
+                start = guess +1; 
+            }
+            else{
+                ans = min(ans ,guess);
+                end = guess-1 ;
             }
         }
-        return ans;
+        if(ans == INT_MAX ) return -1 ;
+        return ans ;
     }
 };
