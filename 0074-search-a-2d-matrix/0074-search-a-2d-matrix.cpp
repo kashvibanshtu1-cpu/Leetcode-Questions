2595cf189256fc771363d8aabe48605116851213
Kashvi 😎 <kashvibanshtu1@gmail.com>
2026-07-29 07:03:51 +0530

    Time: 0 ms (100%), Space: 13.3 MB (79.66%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -1,49 +1,17 @@
 class Solution {
 public:
-    bool searchMatrix(vector<vector<int>>& arr, int tar) {
-        int rowno = arr.size();
-        int colno = arr[0].size();
-        // int start = 0;
-        // int end = rowno - 1;
-        // int row = -1;
-        // while (start <= end) {
-        //     int mid = start + (end - start) / 2;
-        //     if (arr[mid][0] == tar) {
-        //         return true;
-        //     }
-        //     else if (arr[mid][0] > tar) {
-        //         end = mid - 1;
-        //     } else {
-        //         row = mid ;
-        //         start = mid + 1;
-        //     }
-        // }
-        // if(row == -1){
-        //     return false ;
-        // }
-        // int star = 0;
-        // int ed = colno-1;
-        // while (star <= ed) {
-        //     int med = star + (ed - star) / 2;
-        //     if (arr[row][med] == tar) {
-        //         return true;
-        //     } else if (arr[row][med] > tar) {
-        //         ed = med - 1;
-        //     } else {
-        //         star = med + 1;
-        //     }
-        // }
-        // return false;
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int m = matrix.size();
+        int n = matrix[0].size();
         int start = 0;
-        int end = rowno * colno - 1;
+        int end = m * n - 1;
         while (start <= end) {
             int mid = start + (end - start) / 2;
-            int row = mid / colno;
-            int col = mid % colno;
-            if (arr[row][col] == tar){
+            int row = mid / n;
+            int col = mid % n;
+            if (matrix[row][col] == target) {
                 return true;
-            }
-            else if (arr[row][col] > tar) {
+            } else if (matrix[row][col] > target) {
                 end = mid - 1;
             } else {
                 start = mid + 1;
