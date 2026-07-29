6cb1a51d1ef3d997cc580488f62bf5ea582a4722
Kashvi 😎 <kashvibanshtu1@gmail.com>
2026-07-29 06:34:53 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0668-kth-smallest-number-in-multiplication-table/0668-kth-smallest-number-in-multiplication-table.cpp
+++ 0668-kth-smallest-number-in-multiplication-table/0668-kth-smallest-number-in-multiplication-table.cpp
@@ -1,25 +1,24 @@
 class Solution {
 public:
-    int fun(int m, int n, int guess) {
-        int count = 0;
+    int fun(int m, int n, int mid) {
+        int count;
         for (int i = 1; i <= m; i++) {
-            count += min(n, guess / i);
+            count += min(n, mid / i);
         }
-
         return count;
     }
     int findKthNumber(int m, int n, int k) {
         int start = 1;
         int end = m * n;
-        int ans;
+        int ans = -1;
         while (start <= end) {
-            int guess = start + (end - start) / 2;
-            int count = fun(m, n, guess);
+            int mid = start + (end - start) / 2;
+            int count = fun(m, n, mid);
             if (count < k) {
-                start = guess + 1;
+                start = mid + 1;
             } else {
-                ans = guess;
-                end = guess - 1;
+                ans = mid;
+                end = mid - 1;
             }
         }
         return ans;
