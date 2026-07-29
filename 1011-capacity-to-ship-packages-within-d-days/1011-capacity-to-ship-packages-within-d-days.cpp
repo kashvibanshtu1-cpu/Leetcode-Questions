ba91fe2c440ca3b1e65444c3f10a694083a592e7
Kashvi 😎 <kashvibanshtu1@gmail.com>
2026-07-29 05:38:14 +0530

    Time: 13 ms (40.45%), Space: 35 MB (88.95%) - LeetHub

--- 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.cpp
+++ 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.cpp
@@ -1,35 +1,33 @@
 class Solution {
 public:
-    bool isvalid(vector<int>& weight, int k, int mid) {
+    bool capacity(vector<int>& w, int k, int guess) {
+        int weight = w[0];
         int days = 1;
-        int sum = weight[0];
-        for (int i = 1; i < weight.size(); i++) {
-            sum += weight[i];
-            if (sum > mid) {
+         for (int i = 1; i < w.size(); i++) {
+            weight += w[i];
+            if (weight > guess) {
                 days++;
-                sum = weight[i];
+                weight = w[i];
             }
         }
-        
-        if (days > k)
-            return false;
-        return true;
+        if (days <= k)
+            return true;
+        return false;
     }
-    int shipWithinDays(vector<int>& weight, int k) {
-        int start = *max_element(weight.begin(), weight.end());
-        int sum = 0;
-        for (int i = 0; i < weight.size(); i++) {
-            sum += weight[i];
+    int shipWithinDays(vector<int>& w, int k) {
+        int start = *max_element(w.begin(), w.end());
+        int end = 0;
+        for (int i = 0; i < w.size(); i++) {
+            end += w[i];
         }
-        int end = sum;
-        int ans = INT_MAX;
+        int ans = -1;
         while (start <= end) {
-            int mid = start + (end - start) / 2;
-            if (isvalid(weight, k, mid)) {
-                ans = min(ans, mid);
-                end = mid - 1;
+            int guess = start + (end - start) / 2;
+            if (capacity(w, k, guess)) {
+                ans = guess;
+                end = guess - 1;
             } else {
-                start = mid + 1;
+                start = guess + 1;
             }
         }
         return ans;
