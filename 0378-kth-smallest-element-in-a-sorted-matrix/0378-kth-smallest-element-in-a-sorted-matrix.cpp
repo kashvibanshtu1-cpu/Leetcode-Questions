95322eb79aaa9d0be7eaa31c2508c19235d6181e
Kashvi 😎 <kashvibanshtu1@gmail.com>
2026-07-29 06:27:50 +0530

    Time: 0 ms (100%), Space: 17.1 MB (80.5%) - LeetHub

--- 0378-kth-smallest-element-in-a-sorted-matrix/0378-kth-smallest-element-in-a-sorted-matrix.cpp
+++ 0378-kth-smallest-element-in-a-sorted-matrix/0378-kth-smallest-element-in-a-sorted-matrix.cpp
@@ -1,34 +1,33 @@
 class Solution {
 public:
-    int fun(vector<vector<int>>& arr, int guess, int m, int n) {
-        int count =0;
-        int row = m-1 ;
+    int fun(vector<vector<int>>& mat, int mid, int m, int n) {
+        int row = m - 1;
         int col = 0;
-        while(row >=0 && col < n){
-            if(arr[row][col]<=guess){
-                count += row +1 ;
-                col++ ;
-            }
-            else {
+        int count = 0;
+        while (row >= 0 && col < n) {
+            if (mat[row][col] <= mid) {
+                count += row + 1;
+                col++;
+            } else {
                 row--;
             }
         }
-        return count ;
+        return count;
     }
-    int kthSmallest(vector<vector<int>>& arr, int k) {
-        int m = arr.size();
-        int n = arr[0].size();
-        int start = arr[0][0];
-        int end = arr[m - 1][n - 1];
-        int ans;
+    int kthSmallest(vector<vector<int>>& mat, int k) {
+        int m = mat.size();
+        int n = mat[0].size();
+        int start = mat[0][0];
+        int end = mat[m - 1][n - 1];
+        int ans = -1;
         while (start <= end) {
-            int guess = start + (end - start) / 2;
-            int count = fun(arr, guess, m, n);
+            int mid = start + (end - start) / 2;
+            int count = fun(mat, mid, m, n);
             if (count < k) {
-                start = guess + 1;
+                start = mid + 1;
             } else {
-                ans = guess;
-                end = guess - 1;
+                ans = mid;
+                end = mid - 1;
             }
         }
         return ans;
