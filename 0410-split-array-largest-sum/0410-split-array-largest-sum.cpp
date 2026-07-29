f1de26585bacd01c7bd30ef652cd57f3b3c50901
Kashvi 😎 <kashvibanshtu1@gmail.com>
2026-07-29 06:50:48 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0410-split-array-largest-sum/0410-split-array-largest-sum.cpp
+++ 0410-split-array-largest-sum/0410-split-array-largest-sum.cpp
@@ -1,34 +1,33 @@
 class Solution {
 public:
-    bool isvalid(vector<int>& nums, int k, long long  mid) {
-        int maxsum = 0;
-        int part = 1;
+    bool fun(vector<int>& nums, int k, int guess) {
+        int child = 1;
+        int sum = 0;
         for (int i = 0; i < nums.size(); i++) {
-            maxsum += nums[i];
-            if (maxsum > mid) {
-                maxsum = nums[i];
-                part++;
+            sum += nums[i];
+            if (sum > guess) {
+                child++;
+                sum = nums[i];
             }
         }
-        if (part > k )
+        if (child > k)
             return false;
         return true;
     }
     int splitArray(vector<int>& nums, int k) {
-        int start = *max_element(nums.begin(), nums.end());
-        long long sum = 0;
+        int start = *min_element(nums.begin(), nums.end());
+        int end = 0;
         for (int i = 0; i < nums.size(); i++) {
-            sum += nums[i];
+            end += nums[i];
         }
-        int ans;
-        long long  end = sum;
+        int ans = 0;
         while (start <= end) {
-            long long mid = start + (end-start) / 2;
-            if (isvalid(nums, k, mid)) {
-                ans = mid;
-                end = mid - 1;
+            int guess = start + (end - start) / 2;
+            if (fun(nums, k, guess)) {
+                ans = guess;
+                end = guess - 1;
             } else {
-                start = mid + 1;
+                start = guess + 1;
             }
         }
         return ans;
