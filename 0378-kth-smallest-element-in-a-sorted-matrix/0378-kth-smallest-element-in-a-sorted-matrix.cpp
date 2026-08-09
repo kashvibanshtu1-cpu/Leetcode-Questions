dc4d753b1f24940b91fe42fd31f62cc6e55aa681
Kashvi 😎 <kashvibanshtu1@gmail.com>
2026-08-09 13:02:28 +0530

    Time: 0 ms (100%), Space: 17.1 MB (80.63%) - LeetHub

--- 0378-kth-smallest-element-in-a-sorted-matrix/0378-kth-smallest-element-in-a-sorted-matrix.cpp
+++ 0378-kth-smallest-element-in-a-sorted-matrix/0378-kth-smallest-element-in-a-sorted-matrix.cpp
@@ -1,25 +1,36 @@
 class Solution {
 public:
+    int count(vector<vector<int>>& mat, int mid, int m, int n) {
+        int row = m - 1;
+        int col = 0;
+        int count = 0;
+        while (row >= 0 && col < n) {
+            int element = mat[row][col];
+            if (element <= mid) {
+                count += row + 1;
+                col++;
+            } else {
+                row--;
+            }
+        }
+        return count;
+    }
     int kthSmallest(vector<vector<int>>& mat, int k) {
-      	priority_queue<pair<pair<int, int>,int>, vector<pair<pair<int, int>,int>>, greater<pair<pair<int, int>,int>> > pq ;
-		int m = mat.size();
-		int n = mat[0].size();
-		for (int i = 0; i<m; i++) {
-			pq.push({{mat[i][0], i}, 0}); // element,row,col
-		}
-		vector<int> ans ;
-		int row = 0;
-		int col = 0;
-		while (!pq.empty()) {
-			auto t = pq.top();
-			pq.pop();
-			ans.push_back(t.first.first);
-			int row = t.first.second;
-			int col = t.second + 1;
-			if (row <m && col<n) {
-				pq.push({{mat[row][col], row}, col});
-			}
-		}
-		return ans[k-1] ;  
+        int m = mat.size();
+        int n = mat[0].size();
+        int start = mat[0][0];
+        int end = mat[m - 1][n - 1];
+        int ans = INT_MAX;
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            int c = count(mat, mid,m,n);
+            if (c >= k) {
+                ans = min(mid, ans);
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            }
+        }
+        return ans;
     }
 };
