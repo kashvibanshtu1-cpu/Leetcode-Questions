e0b89695cc4d97ad4fe6b81d052211c9d88733f2
Kashvi 😎 <kashvibanshtu1@gmail.com>
2026-08-10 06:49:41 +0530

    Time: 6 ms (12.95%), Space: 16.4 MB (12.35%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.cpp
+++ 0022-generate-parentheses/0022-generate-parentheses.cpp
@@ -1,27 +1,25 @@
 class Solution {
 public:
-    void valid(int n, vector<string>& ans, string diary, int open, int close) {
+    void gen(vector<string>& ans, string temp, int n, int open, int close) {
         if (open == n && close == n) {
-            ans.push_back(diary);
+            ans.push_back(temp);
             return;
         }
-        // add (
         if (open < n) {
-            diary.push_back('(');
-            valid(n, ans, diary, open + 1, close);
-            diary.pop_back();
+            temp.push_back('(');
+            gen(ans, temp, n, open + 1, close);
+            temp.pop_back();
         }
-        // add )
-        if (close < open) {
-            diary.push_back(')');
-            valid(n, ans, diary, open, close + 1);
-            diary.pop_back();
+        if (close < open && close < n) {
+            temp.push_back(')');
+            gen(ans, temp, n, open, close + 1);
+            temp.pop_back();
         }
     }
     vector<string> generateParenthesis(int n) {
         vector<string> ans;
-        string diary = "";
-        valid(n, ans, diary, 0, 0);
+        string temp = "";
+        gen(ans, temp, n, 0, 0);
         return ans;
     }
 };
