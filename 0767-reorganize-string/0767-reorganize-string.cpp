ca320c63c8a51d42a449e36146f31db4d72f3483
Kashvi 😎 <kashvibanshtu1@gmail.com>
2026-08-10 08:00:25 +0530

    Time: 3 ms (14.68%), Space: 8.3 MB (80.41%) - LeetHub

--- 0767-reorganize-string/0767-reorganize-string.cpp
+++ 0767-reorganize-string/0767-reorganize-string.cpp
@@ -3,55 +3,67 @@ public:
     string reorganizeString(string s) {
 
         unordered_map<char, int> mp;
+        string ans = "";
 
-        for (char ch : s)
-            mp[ch]++;
+        // Count frequency
+        for (char c : s) {
+            mp[c]++;
+        }
 
+        // Max heap
         priority_queue<pair<int, char>> pq;
 
         for (auto p : mp) {
             pq.push({p.second, p.first});
         }
 
-        string ans = "";
-        int curr = 0;
+        for (int i = 0; i < s.size(); i++) {
 
-        while (!pq.empty()) {
-
-            pair<int, char> t = pq.top();
+            auto t = pq.top();
             pq.pop();
 
             int freq = t.first;
+            char val = t.second;
 
-            if (curr == 0 || ans[curr - 1] != t.second) {
+            // If different from previous character
+            if (ans.empty() || ans.back() != val) {
 
-                ans.push_back(t.second);
+                ans.push_back(val);
+
                 freq--;
 
-                if (freq > 0)
-                    pq.push({freq, t.second});
+                if (freq > 0) {
+                    pq.push({freq, val});
+                }
 
-            } else {
-
-                if (pq.empty())
-                    return "";
-
-                pair<int, char> snd = pq.top();
-                pq.pop();
-
-                int freq2 = snd.first;
-
-                ans.push_back(snd.second);
-                freq2--;
-
-                if (freq2 > 0)
-                    pq.push({freq2, snd.second});
-
-                // Push the skipped character back
-                pq.push({freq, t.second});
             }
 
-            curr++;
+            // If same as previous character
+            else {
+
+                // No alternative character
+                if (pq.empty()) {
+                    return "";
+                }
+
+                auto t2 = pq.top();
+                pq.pop();
+
+                int freq2 = t2.first;
+                char val2 = t2.second;
+
+                // Use second character
+                ans.push_back(val2);
+
+                freq2--;
+
+                if (freq2 > 0) {
+                    pq.push({freq2, val2});
+                }
+
+                // We did NOT use val, so put it back unchanged
+                pq.push({freq, val});
+            }
         }
 
         return ans;
