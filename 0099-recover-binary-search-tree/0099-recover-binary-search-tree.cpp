776535f4b5938c3881b0b832a3403ad8a77819c2
Kashvi 😎 <kashvibanshtu1@gmail.com>
2026-08-15 05:37:36 +0530

    Time: 0 ms (100%), Space: 60.9 MB (52.83%) - LeetHub

--- 0099-recover-binary-search-tree/0099-recover-binary-search-tree.cpp
+++ 0099-recover-binary-search-tree/0099-recover-binary-search-tree.cpp
@@ -1,15 +1,3 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
- * right(right) {}
- * };
- */
 class Solution {
 public:
     TreeNode* g_f = NULL;
@@ -17,29 +5,38 @@ public:
     TreeNode* g2_f = NULL;
     TreeNode* g2_s = NULL;
     TreeNode* prev = NULL;
+
     void fun(TreeNode* root, int& count) {
-        if (root == NULL) {
+        if (root == NULL)
             return;
-        }
+
         fun(root->left, count);
+
         if (prev != NULL && prev->val > root->val && count >= 1) {
             g2_f = prev;
             g2_s = root;
             count++;
         }
+
         if (prev != NULL && prev->val > root->val && count == 0) {
             g_f = prev;
             g_s = root;
             count++;
         }
+
         prev = root;
+
         fun(root->right, count);
     }
+
     void recoverTree(TreeNode* root) {
         int count = 0;
+
         fun(root, count);
-        if (count == 1)
+
+        if (count == 1) {
             swap(g_f->val, g_s->val);
+        }
         else {
             swap(g_f->val, g2_s->val);
         }
