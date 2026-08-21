96fe852ca93873fd8af9bbb35147b4474ba0777f
Kashvi 😎 <kashvibanshtu1@gmail.com>
2026-08-21 17:07:32 +0530

    Time: 23 ms (83.78%), Space: 16.6 MB (65.34%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -2,38 +2,37 @@ class Solution {
 public:
     vector<int> x = {-1, 1, 0, 0};
     vector<int> y = {0, 0, -1, 1};
-    bool valid(int i, int j, int m, int n) {
-        if (i >= 0 && i < m && j >= 0 && j < n) {
+    bool valid(int row, int col, int m, int n) {
+        if (row >= 0 && col >= 0 && row < m && col < n)
             return true;
-        }
         return false;
     }
-    void dfs(vector<vector<char>>& a, vector<vector<bool>>& vis, int m, int n,
-             int i, int j) {
-        vis[i][j] = true;
-        for (int k = 0; k < x.size(); k++) {
-            int row = i + x[k];
-            int col = j + y[k];
-            if (valid(row, col, m, n) && a[row][col] == '1' &&
-                vis[row][col] == false) {
-                dfs(a, vis, m, n, row, col);
+    void dfs(vector<vector<char>>& grid, vector<vector<bool>>& visited, int row,  int col, int m, int n) {
+        visited[row][col] = true;
+        for (int i = 0; i < 4; i++) {
+            int nrow = row + x[i];
+            int ncol = col + y[i];
+            if (valid(nrow, ncol, m, n) && !visited[nrow][ncol] &&
+                grid[nrow][ncol] == '1') {
+                dfs(grid, visited, nrow, ncol, m, n);
             }
         }
         return;
     }
-    int numIslands(vector<vector<char>>& a) {
-        int m = a.size();
-        int n = a[0].size();
-        int res = 0;
-        vector<vector<bool>> vis(m, vector<bool>(n, false));
+
+    int numIslands(vector<vector<char>>& grid) {
+        int m = grid.size();
+        int n = grid[0].size();
+        vector<vector<bool>> visited(m, vector<bool>(n, false));
+        int count = 0;
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
-                if (a[i][j] == '1' && vis[i][j] == false) {
-                    dfs(a, vis, m, n, i, j);
-                    res++;
+                if (grid[i][j] == '1' && visited[i][j] == false) {
+                    dfs(grid, visited, i, j, m, n);
+                    count++;
                 }
             }
         }
-        return res;
+        return count;
     }
 };
